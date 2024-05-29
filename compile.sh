b469ad4adec78f1944ec8d7dd64404ce534ea0c4
Varga Peter <varga.peter2500@gmail.com>
2024-05-29 16:53:54 +0200

    built executables

--- compile.sh
+++ compile.sh
@@ -3,9 +3,9 @@
 CPP_FLAGS="-std=c++20 -O3 -static"
 SRC_DIR=./src/*.cpp
 COMPRESSOR=./compressor_main.cpp
-COMPRESSOR_NAME=compressor
+COMPRESSOR_NAME=./exec_linux/compressor
 DECOMPRESSOR=./decompressor_main.cpp
-DECOMPRESSOR_NAME=decompressor
+DECOMPRESSOR_NAME=./exec_linux/decompressor
 
 g++ $CPP_FLAGS $SRC_DIR $COMPRESSOR -o $COMPRESSOR_NAME > compressor_compile.log 2>&1
 
